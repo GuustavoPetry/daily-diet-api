248c93e868dc9ee5fa3728af5b5faa84d98ff81f
GuustavoPetry <ogustavopetry@gmail.com>
2026-01-06 09:47:59 -0300

    Ambiente inicial API

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import fastify from "fastify";
+import { userRoutes } from "./routes/user-routes";
+
+export const app = fastify();
+
+app.register(userRoutes, {
+    prefix: "user"
+});
---
+++ src/env/index.ts
@@ -0,0 +1,31 @@
+import { config } from "dotenv";
+import { z } from "zod";
+
+// Condicional para apontar path-env com base em na variável NODE_ENV
+switch (process.env.NODE_ENV) {
+    case "test":
+        config({ path: ".env.test" });
+        break;
+    default:
+        config();
+}
+
+// Criar schema para validar as variáveis de ambiente
+const envSchema = z.object({
+    NODE_ENV: z.enum(["development", "test", "production"]),
+    DATABASE_CLIENT: z.enum(["sqlite", "pg"]),
+    DATABASE_URL: z.string(),
+    PORT: z.coerce.number().default(3333)
+});
+
+// Validar o schema criado
+const _env = envSchema.safeParse(process.env);
+
+// Se o schema não aprovar, throw error
+if (_env.success === false) {
+    console.log("Invalid Environment Variables", z.treeifyError(_env.error).properties);
+    throw new Error("Invalid Environment Variables");
+}
+
+// exportar variável com as variáveis de ambiente
+export const env = _env.data;
