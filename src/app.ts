1fce476fcd4cd159860e2c9c607837c60ec64aa8
GuustavoPetry <ogustavopetry@gmail.com>
2026-01-12 12:18:23 -0300

    Registrando fastifyJwt e Protegendo Rotas com decorate

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
-import fastify from "fastify";
-import { userRoutes } from "./routes/user-routes";
+import fastify, { FastifyReply, FastifyRequest } from "fastify";
+import fastifyJwt from "@fastify/jwt";
+import { userRoutes } from "./routes/user-routes.js";
+import { authRoutes } from "./routes/auth-routes.js";
+import { env } from "./env/index.js";
 
 export const app = fastify();
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET
+});
+
+app.decorate("authenticate", async (request: FastifyRequest) => {
+  await request.jwtVerify();
+});
+
+app.register(authRoutes, {
+    prefix: "auth"
+});
 
 app.register(userRoutes, {
     prefix: "user"
