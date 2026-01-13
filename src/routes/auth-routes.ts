19a8d9b3cf262000076f226c58acf0d263dc68bc
GuustavoPetry <ogustavopetry@gmail.com>
2026-01-13 08:31:30 -0300

    rote de autenticação -> retorna jwt

---
+++ src/routes/auth-routes.ts
@@ -0,0 +1,43 @@
+import { FastifyInstance } from "fastify";
+import { knex } from "../database.js";
+import { User } from "../interfaces/User.js";
+import { compare } from "bcrypt-ts";
+
+export async function authRoutes(app: FastifyInstance) {
+    // Rota de autenticação -> retorna jwtToken
+    app.post("/signup", async (request, reply) => {
+        const { email, password } = request.body as {
+            email: string,
+            password: string
+        };
+
+        // Fazer verificação se o usuário e senha estão corretos com bcrypt decode...
+        const user = await knex<User>("users")
+        .select()
+        .where({
+            email,
+        })
+        .first();
+        
+        if (!user) {
+            return reply.send({ erro: "Credenciais Inválidas" });
+        };
+        
+        const isValid = await compare(password, user.password);
+
+        if (!isValid) {
+            return reply.send({ erro: "Credenciais Inválidas" });
+        }
+
+        const token = app.jwt.sign(
+            {
+                email: user.email,
+            },
+            {
+                sub: user.id,
+                expiresIn: 3600
+            });
+
+        return reply.send({ token });
+    });
+}
