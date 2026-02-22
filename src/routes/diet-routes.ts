fb44d069fd43bad5e6ffbec6e1c468212260ff28
GuustavoPetry <ogustavopetry@gmail.com>
2026-02-22 09:10:04 -0300

    Crud Refeições

--- src/routes/diet-routes.ts
+++ src/routes/diet-routes.ts
@@ -169,4 +169,100 @@ export async function dietRoutes(app: FastifyInstance) {
             return reply.status(201).send({ result });
         }
     );
+
+    app.delete<{
+        Body: {
+            id: string
+        }
+    }>
+        ("/diet.delete", {
+            preHandler: [app.authenticate],
+            schema: {
+                body: {
+                    type: "object",
+                    required: ["id"],
+                    properties: {
+                        id: { type: "string" }
+                    }
+                }
+            }
+        },
+            async (request, reply) => {
+                const { id } = request.body;
+
+                const user_id = request.user.sub;
+
+                const result = await knex("diet")
+                    .delete()
+                    .where({
+                        id,
+                        user_id
+                    });
+
+                return reply.status(200).send({ result });
+            }
+        );
+
+    app.get("/diet.metrics", {
+        preHandler: [app.authenticate],
+    },
+        async (request, reply) => {
+            const user_id = request.user.sub;
+
+            const total_meals = await knex("diet")
+                .select()
+                .count("id")
+                .where({
+                    user_id
+                })
+                .first();
+
+            const total_isDiet = await knex("diet")
+                .count("id")
+                .where({
+                    user_id,
+                    isDiet: true
+                })
+                .first();
+
+            const total_noDiet = await knex("diet")
+                .count("id")
+                .where({
+                    user_id,
+                    isDiet: false
+                })
+                .first();
+
+            const selectMeals = await knex("diet")
+                .select("isDiet", "created_at")
+                .where({
+                    user_id,
+                });
+
+            let sum = 0;
+            let best_sequence_isDiet = 0;
+            let allIsDiet = true;
+
+            selectMeals.forEach(item => {
+                if (item.isDiet === 1) sum += 1;
+
+                else {
+                    best_sequence_isDiet = sum;
+                    allIsDiet = false;
+                    sum = 0;
+                };
+            });
+
+            if (allIsDiet || sum > best_sequence_isDiet) best_sequence_isDiet = sum;
+
+            return reply.status(200).send({
+                metrics: {
+                    total_meals: total_meals!["count(`id`)"],
+                    total_isDiet: total_isDiet!["count(`id`)"],
+                    total_noDiet: total_noDiet!["count(`id`)"],
+                    best_sequence_isDiet
+                }
+            });
+        }
+    );
 }
